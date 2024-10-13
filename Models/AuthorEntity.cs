f76191b705e5e0c5cd995276409099995c4560bd
user <user@WIN-MKQ26SV0D4C>
2024-10-13 16:53:35 +0300

    Добавьте файлы проекта.

---
+++ Models/AuthorEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_15.Models
+{
+    public class AuthorEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Models/BookEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_15.Models
+{
+    public class BookEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Guid AuthorId { get; set; }
+        public AuthorEntity Author { get; set; }
+        public Guid GenreId { get; set; }
+        public GenreEntity Genre { get; set; }
+        public int PublishedYear { get; set; }
+    }
+}
